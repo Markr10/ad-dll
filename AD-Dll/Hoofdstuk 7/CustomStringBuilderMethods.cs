54dadefafcdf2c16edaee3f7ad06abc4ede58e8a
Mark <mark-r@live.nl>
2014-05-07 15:06:57 +0200

    Commentaar toegevoegd

--- AD-Dll/Hoofdstuk 7/CustomStringBuilderMethods.cs
+++ AD-Dll/Hoofdstuk 7/CustomStringBuilderMethods.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace AD_Dll.Hoofdstuk_7
 {
+    /// <summary>
+    /// Mark Roelans
+    /// Zelfgemaakte methode(s) die gebruikt maken van StringBuilder objecten.
+    /// </summary>
     public class CustomStringBuilderMethods
     {
+        /// <summary>
+        /// Print informatie over een StringBuilder object.
+        /// </summary>
+        /// <param name="stBuff">Het StringBuilder object waarover informatie moet worden uitgeprint.</param>
+        /// <param name="name">De naam van het StringBuilder object.</param>
         public static void PrintInfo(StringBuilder stBuff, string name)
         {
             Console.WriteLine("Length of {0}: {1}", name, stBuff.Length);
