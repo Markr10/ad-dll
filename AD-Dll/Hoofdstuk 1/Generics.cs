49d6e39be9b7bd7320ca743f6c5b7bb894a3a08f
raymonnetje <raymon.bunt@student.stenden.com>
2014-05-07 11:54:37 +0200

    comments added

--- AD-Dll/Hoofdstuk 1/Generics.cs
+++ AD-Dll/Hoofdstuk 1/Generics.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace AD_Dll.Hoofdstuk_1
 {
+    /// <summary>
+    /// Raymon Bunt
+    /// deze klasse maakt een list aan waar je feitelijk alles in kan opslaan
+    /// </summary>
+    /// <typeparam name="T">T staat voor dat je alle datatypen kan opslaan</typeparam>
     public class Generics<T> where T : IComparable
     {
         List<T> list = new List<T>();
 
+        /// <summary>
+        /// item toevoegen aan de list
+        /// </summary>
+        /// <param name="item">het item dat je aan de list gaat toevoegen</param>
         public void addToList(T item)
         {
             list.Add(item);
