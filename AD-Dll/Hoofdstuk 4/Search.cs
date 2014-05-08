a35b648b66ca826789df481bf826d1939b30232e
Mark <mark-r@live.nl>
2014-05-08 10:26:20 +0200

    Commentaar verbeterd

--- AD-Dll/Hoofdstuk 4/Search.cs
+++ AD-Dll/Hoofdstuk 4/Search.cs
@@ -6,33 +6,38 @@ using System.Threading.Tasks;
 
 namespace AD_Dll.Hoofdstuk_4
 {
+    /// <summary>
+    /// Thomas Oldengarm
+    /// Search klasse
+    /// </summary>
+    /// <typeparam name="T">Het type gegevens dat is opgeslagen in de klasse</typeparam>
     public class Search<T> where T : IComparable<T>
     {
        /// <summary>
-        /// lineare zoekfuntie
+       /// Lineare zoekfuntie
        /// </summary>
-       /// <param name="a">array</param>
-       /// <param name="v">wat je zoekt</param>
-       /// <returns>index</returns>
+       /// <param name="a">De array waarin gezocht moet worden</param>
+       /// <param name="v">Het item wat gezocht moet worden</param>
+       /// <returns>De index van het item in de array. -1 als het item niet gevonden is</returns>
         public static int linear(T[] a, T v) 
         {
             for (int i = 0; i < a.Length; ++i)
             {
-                if (v.Equals(a[i])) //als gevonden is, return de index nummer
+                if (v.Equals(a[i])) // als gevonden is, return de index nummer
                 {
                     Console.WriteLine("Found at index: ");
                     return i;
                 }
             }
-            return -1; //niet gevonden, return -1
+            return -1; // niet gevonden, return -1
         }
 
         /// <summary>
-        /// //binaire zoekfunctie
+        /// Binaire zoekfunctie
         /// </summary>
-        /// <param name="a">array</param>
-        /// <param name="v">wat je zoekt</param>
-        /// <returns>index</returns>
+        /// <param name="a">De array waarin gezocht moet worden</param>
+        /// <param name="v">Het item wat gezocht moet worden</param>
+        /// <returns>De index van het item in de array. -1 als het item niet gevonden is</returns>
         public static int binary(T[] a, T v) 
         {
             int low = 0;
@@ -40,30 +45,27 @@ namespace AD_Dll.Hoofdstuk_4
 
             while (low <= high)
             {
-                int mid = (low + high) / 2; //zoek het midden van de array
+                int mid = (low + high) / 2; // zoek het midden van de array
                 T midVal = a[mid];
                 int result = v.CompareTo(midVal);
 
-                if (result < 0) //key ligt lager
+                if (result < 0) // key ligt lager
                 {
                     high = mid - 1;
                 }
 
-                else if (result > 0) //key ligt hoger
+                else if (result > 0) // key ligt hoger
                 {
                     low = mid + 1;
                 }
 
-                else //key gevonden, return index
+                else // key gevonden, return index
                 {
                     Console.WriteLine("Found at index: ");
                     return mid;
                 }
             }
-            return -1; //key niet gevonden
+            return -1; // key niet gevonden
         }
     }
 }
-
-
-
