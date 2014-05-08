09aada8ae52ab2cdd18104d90a342f5fb7d0a017
Mark <mark-r@live.nl>
2014-05-08 11:32:04 +0200

    Unused usings removed and usings sorted. Now with all changed files.

--- AD-Dll/Hoofdstuk 5/Stacks.cs
+++ AD-Dll/Hoofdstuk 5/Stacks.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace AD_Dll.Hoofdstuk_5
 {
--- AD-Dll/Other.cs
+++ AD-Dll/Other.cs
@@ -1,9 +1,5 @@
-﻿using AD_Dll.Hoofdstuk_1;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AD_Dll
 {
